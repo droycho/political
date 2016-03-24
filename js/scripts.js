3a78b8762f7c39a8c82ee3f65ed9b992a8d42ca9
DJ Roychoudhury and Joshua Logan <dj@>
2016-03-24 11:47:23 -0700

    added images and radio boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,33 @@
 
 $(document).ready(function() {
   $("form").submit(function(event) {
-    var age = parseInt($("input#age").val());
-    console.log(age);
 
-    if (age > 18) {
-      window.location.href = "over18.html";
-    } else if (age === 18){
-        window.location.href = "exact18.html";
-      } else {
-        window.location.href = "under18.html";
+    var score= 0;
+
+    var result= parseInt($("#party").val());
+    score += result;
+    var result= parseInt($("#president").val());
+    score += result;
+    var result= parseInt($("#tax").val());
+    score += result;
+    var result= parseInt($("#social").val());
+    score += result;
+    var result= parseInt($("#foreign").val());
+    score += result;
+    var result= parseInt($("#economic").val());
+    score += result;
+    var result= parseInt($("#looks").val());
+    score += result;
+    console.log(score);
+
+    if (score < 60) {
+      alert("Conservative");
+    } else if (score === 60) {
+      alert("Independent");
+    } else {
+      alert("Democrat")
     }
+
     event.preventDefault();
   });
 });
